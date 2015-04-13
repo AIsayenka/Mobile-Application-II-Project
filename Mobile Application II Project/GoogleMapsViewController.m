74e0d99fca1fa7cea2e63f5233d8e0ff96deee1b
Aliaksei Isayenka <aliaksei.isayenka@gmail.com>
2015-04-12 23:53:24 -0400

    Struggles with google maps

--- Mobile Application II Project/GoogleMapsViewController.m
+++ Mobile Application II Project/GoogleMapsViewController.m
@@ -14,14 +14,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.868
-                                                            longitude:151.2086
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:43.7182412
+                                                            longitude:-79.378058
+                                                                 zoom:12];
     
     self._mapsView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    
+    //self._mapsView.myLocationEnabled = YES;
     DBManager* dbconnection = [[DBManager alloc] init];
-    
+    [self.view addSubview:self._mapsView];
     NSMutableArray* data = [[NSMutableArray alloc]init];
     data = [dbconnection selectDistinct];
     //if([dbconnection createDB])
@@ -31,14 +31,15 @@
             
 
             CLLocationCoordinate2D position = { [data[i][5] doubleValue], [data[i][6] doubleValue] };
-            GMSMarker *marker = [GMSMarker markerWithPosition:position];
-            marker.title = [NSString stringWithFormat:@"Actor: %@\nStatement ID: %@", data[i][2], data[i][1]];
-            marker.appearAnimation = YES;
-            marker.flat = YES;
-            marker.snippet = @"";
-            marker.map = self._mapsView;
+            GMSMarker *_marker = [GMSMarker markerWithPosition:position];
+            _marker.title = [NSString stringWithFormat:@"Actor: %@\nStatement ID: %@", data[i][2], data[i][1]];
+            //marker.appearAnimation = YES;
+            //marker.flat = YES;
+            _marker.snippet = [NSString stringWithFormat:@"Marker #%i", i];
+            _marker.map = self._mapsView;
         
         }
+    
     //}
 }
 
